fc7dd0541b30692952c5b4c5f8e4ca17a151032f
Tata11jr <W.pannawich@gmail.com>
2024-03-25 17:19:02 +0700

    fix car catalog

--- src/components/CarsCatalog.tsx
+++ src/components/CarsCatalog.tsx
@@ -1,16 +1,16 @@
 import Card from "./Card";
 import Link from "next/link";
 
-export default async function CarCatalog({carsJson} : {hospitalsJson:Promise<CarsJson>}) {
+export default async function CarCatalog({carsJson} : {carsJson:Promise<CarsJson>}) {
 
     const carsJsonReady = await carsJson
 
     return (
         <div style={{margin:"20px", display:"flex", flexDirection:"row", flexWrap:"wrap", justifyContent:"space-around", alignContent:"space-around"}}>
                 {
-                    carsJsonReady.data.map((carsItem:any) => (
-                        <Link href={`/hospital/${carsItem.id}`} className="w-1/5" key={carsItem.name}>
-                        <Card carName={carsItem.name} imgSrc={carsItem.picture}/>
+                    carsJsonReady.data.map((carsItem:CarsItem) => (
+                        <Link href={`/hospital/${carsItem.id}`} className="w-1/5 mb-10" key={carsItem.model}>
+                        <Card carName={carsItem.model} imgSrc={carsItem.picture}/>
                         </Link>
                     ))
                 }
